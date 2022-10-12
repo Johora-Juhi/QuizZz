3dd525675d1e6cf1ce12f89f9a077581592ed2aa
Johora-Juhi <zohrajuhi1012@gmail.com>
2022-10-12 19:45:33 +0600

    quiz

--- src/Componant/Quiz/Quiz.js
+++ src/Componant/Quiz/Quiz.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 import './Quiz.css'
 
 const Quiz = () => {
+    const quizs = useLoaderData();
+    const question = quizs.data.questions[0].question;
+    const x = quizs.data.questions;
     return (
         <div>
-            
+            {
+                x.map(y => <p>{y.question}</p>)
+            }
         </div>
     );
 };
