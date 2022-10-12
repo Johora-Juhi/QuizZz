bb9e8d54bc5cfdd4ea5fc04e379632698f542f97
Johora-Juhi <zohrajuhi1012@gmail.com>
2022-10-12 23:20:38 +0600

    header logo

--- src/Componant/Answer/Answer.js
+++ src/Componant/Answer/Answer.js
@@ -5,21 +5,21 @@ import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const Answer = ({quiz}) => {
+const Answer = ({ quiz }) => {
     const [show, setShow] = useState(false);
     const showToastMessage = (myAns) => {
         if (myAns === quiz.correctAnswer) {
-        toast.success('Right Answer !', {
-            position: toast.POSITION.TOP_CENTER
-        });
-    }
-    else{
-        toast.error('Wrong Answer !', {
-            position: toast.POSITION.TOP_CENTER
-        });
-    }
+            toast.success('Right Answer !', {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
+        else {
+            toast.error('Wrong Answer !', {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
     };
-    
+
     return (
         <div className='quiz-question'>
             <h3>{quiz.question}</h3>
@@ -27,7 +27,7 @@ const Answer = ({quiz}) => {
                 {quiz.options.map(eachOptions =>
                     <div>
                         <h6 onClick={() => showToastMessage(eachOptions)}>{eachOptions}</h6>
-                    <ToastContainer/>
+                        <ToastContainer />
                     </div>
                 )}
             </div>
--- src/Componant/Blog/Blog.js
+++ src/Componant/Blog/Blog.js
@@ -6,15 +6,15 @@ const Blog = () => {
         <div className="container">
             <h1 className='py-3 border-bottom'>Blogs</h1>
             <div  className="bg-light my-4 py-3 px-5 border rounded box">
-                <h2 className=''>What is the purpose of React Router?</h2>
+                <h2 className=''>--What is the purpose of React Router?--</h2>
                 <h6>React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL. React Router, and dynamic, client-side routing, allows us to build a single-page web application with navigation without the page refreshing as the user navigates. React Router uses component structure to call components, which display the appropriate information.</h6>
             </div>
             <div className="bg-light my-4 py-3 px-5 border rounded box">
-                <h2 >How does Context API works?</h2>
+                <h2 >--How does Context API works?--</h2>
                 <h6>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. React's context allows you to share information to any component, by storing it in a central place and allowing access to any component that requests itusually you are only able to pass data from parent to child via props.</h6>
             </div>
             <div className="bg-light my-4 py-3 px-5 border rounded box">
-                <h2 >Discussion about useRef</h2>
+                <h2 >--Discussion about useRef--</h2>
                 <h6>The useRef Hook allows to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly. If we tried to count how many times our application renders using the useState Hook, we would be caught in an infinite loop since this Hook itself causes a re-render. To avoid this, we can use the useRef Hook.</h6>
             </div>
         </div>
