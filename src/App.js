f36e55164928c06cc1d60b0e0c0ac7f060e37967
Johora-Juhi <zohrajuhi1012@gmail.com>
2022-10-12 13:24:12 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './Layouts/Main';
+import Home from './Componant/Home/Home';
+import Statics from './Componant/Statics/Statics';
+import Blog from './Componant/Blog/Blog';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Home></Home>
+        },
+        {
+          path: '/statistics',
+          element: <Statics></Statics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        },
+      ]
+    }
+  ])
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/Componant/Header/Header.js
+++ src/Componant/Header/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='header'>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand className='brand-name'>Let's Test</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Link to="/">Topics</Link>
+                            <Link to="/statistics">Statistics</Link>
+                            <Link to="/blog">Blog</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
